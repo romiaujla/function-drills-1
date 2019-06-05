31344f9fc6b0df42a714997266a45ddd7896379f
Raman Aujla <ramanaujla@Ramneets-MacBook-Air.local>
2019-06-05 16:30:58 -0400

    "Completed decode function"

--- functiondrills2.js
+++ functiondrills2.js
@@ -17,3 +17,40 @@ console.log(beyond(1));
 console.log(beyond(-1));
 console.log(beyond(0));
 
+
+function decode(word){
+  let firstChar = word.slice(0,1);
+  switch(firstChar){
+    case 'a':
+      return word[1];
+      break;
+    case 'b':
+      return word[2];
+      break;
+    case 'c':
+      return word[3];
+      break;
+    case 'd':
+      return word[4]
+      break;
+    default:
+      return " ";
+      break;
+  }
+}
+
+
+let str = "craft block argon meter bells brown croon droop";
+let strArray = str.split(" ");
+
+for(let i = 0; i < strArray.length; i++){
+  console.log(decode(strArray[i]));
+}
+
+//Cycle = 'l'
+//12345
+//apple = 'p'
+//12345
+//mouse = ' '
+//craft block argon meter bells brown croon droop
+//   f    o    r    ' '     l     o      o      p
